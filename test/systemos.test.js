35451c999a7d21a6476ee62c301b570815badcde
ikm216 <ikmclassof22@gmail.com>
2024-12-06 21:02:52 -0500

    test: added test to get total memory and system architecture.

--- test/systemos.test.js
+++ test/systemos.test.js
@@ -9,4 +9,10 @@ describe('Os Functions', () => {
 
         expect(numberCPUs()).toBe(5);
     });
+    test('should return system architecture and total memory ', () => {
+        os.arch.mockReturnValue('x64');
+        os.totalmem.mockReturnValue(16 * 1024 ** 3);
+        const details = getSystemDetails();
+        expect(details).toEqual({ architecture: 'x64', totalMemoryGB: '16.00' });
+    });
 });
