9b0e3a9bc6188ff69ccb6631b986f57d421f5b4c
ikm216 <ikmclassof22@gmail.com>
2024-12-06 21:21:28 -0500

    test: added test to check if enough memory is available.

--- test/systemos.test.js
+++ test/systemos.test.js
@@ -15,4 +15,9 @@ describe('Os Functions', () => {
         const details = getSystemDetails();
         expect(details).toEqual({ architecture: 'x64', memory: '16.00' });
     });
+    test('should return if enough memory is available', () => {
+        os.freemem.mockReturnValue(2 * 1024 ** 3); 
+        expect(hasMemory(1000)).toBe(true); 
+        expect(hasMemory(3000)).toBe(false);
+    })
 });
