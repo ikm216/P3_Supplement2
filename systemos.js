f16f050da11aa093e951924bbe1bf4435129d051
ikm216 <ikmclassof22@gmail.com>
2024-12-06 20:54:20 -0500

    doc: added documentation to get number of CPUs.

--- systemos.js
+++ systemos.js
@@ -1,5 +1,9 @@
 const os = require('os');
 
+/**
+ * Returns the number of CPUs available on the system.
+ * @returns {number} The number of CPUs.
+ */
 function numberCPUs() {
     return os.cpus().length;
 }
